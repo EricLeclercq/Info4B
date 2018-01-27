06114c009e64f6eaa73b15d348173a7d0f591ab9
Eric Leclercq <Eric.Leclercq@u-bourgogne.fr>
2018-01-27 17:46:48 +0100

    Banque avec verrou

---
+++ BankSimulation/Banque02.java
@@ -0,0 +1,56 @@
+import java.util.concurrent.locks.*;
+
+class Compte{
+ public Lock l;
+ private int solde;
+ public Compte(int solde){
+  this.solde=solde;
+  l=new ReentrantLock();
+ }
+ int getSolde(){ return solde;}
+ void setSolde(int solde){this.solde=solde;}
+}
+
+class Distributeur extends Thread{
+ private int montant;
+ private int nbRetrait;
+ private Compte c;
+ public Distributeur(Compte c, int montant, int nbRetrait){
+  this.c=c;
+  this.montant=montant;
+  this.nbRetrait=nbRetrait;
+ }
+ public void run(){
+  for(int j=1; j<=nbRetrait ; j++){
+   int temp;
+   c.l.lock();
+   // debut section critique
+   temp=c.getSolde();
+   temp=temp-montant;
+   c.setSolde(temp);
+   // fin section critique
+   c.l.unlock(); 
+ }
+ } 
+}
+
+
+public class Banque02{
+
+public static void main(String[] arg){
+
+  Compte c = new Compte(800000);
+  Distributeur d1=new Distributeur(c,10,1000);
+  Distributeur d2=new Distributeur(c,50,1000);
+
+  d1.start();
+  d2.start();
+
+try{
+  d1.join();
+  d2.join();
+}catch (InterruptedException e){e.printStackTrace();}
+
+ System.out.println("Solde final="+c.getSolde());
+}
+}
