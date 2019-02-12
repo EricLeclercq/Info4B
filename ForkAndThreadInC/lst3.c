0ed60cdaec103f907b16120308fa3b963c853a3e
Eric LECLERCQ <leclercq@u-bourgogne.fr>
2019-02-12 15:13:08 +0100

    Create lst3.c

---
+++ ForkAndThreadInC/lst3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *my_thread_process (void * arg)
+{
+  int i;
+
+  for (i = 0 ; i < 5 ; i++) {
+    printf ("Thread %s: %d\n", (char*)arg, i);
+    sleep (1);
+  }
+  pthread_exit (0);
+}
+
+main (int ac, char **av)
+{
+  pthread_t th1, th2;
+  void *ret;
+
+  if (pthread_create (&th1, NULL, my_thread_process, "1") < 0) {
+    fprintf (stderr, "pthread_create error for thread 1\n");
+    exit (1);
+  }
+
+  if (pthread_create (&th2, NULL, my_thread_process, "2") < 0) {
+    fprintf (stderr, "pthread_create error for thread 2\n");
+    exit (1);
+  }
+
+  (void)pthread_join (th1, &ret);
+  (void)pthread_join (th2, &ret);
+}
